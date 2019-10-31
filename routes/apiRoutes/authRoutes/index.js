7c3eee8aa16db1e47e23c5e66842a3420d60fa6d
emmanueljucaban <emmanueljucaban@gmail.com>
2019-10-31 14:31:02 -0700

    Add stuff

--- routes/apiRoutes/authRoutes/index.js
+++ routes/apiRoutes/authRoutes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const authController = require('./../../../controllers/authController');
 
 // api/auth prepended to every route declared in here
 
@@ -7,10 +7,10 @@ router.route('/signup')
     .post(authController.signUp);
 
 
-router.route('/signin')
-    .post(authController.signIn);
+// router.route('/signin')
+//     .post(authController.signIn);
 
-router.route('/logout')
-    .get(authController.logout);
+// router.route('/logout')
+//     .get(authController.logout);
 
 module.exports = router;
