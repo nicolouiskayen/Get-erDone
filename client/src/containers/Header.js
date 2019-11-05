ba3bc3bbd459bc58323197cce94cbb8d0e1438da
emmanueljucaban <emmanueljucaban@gmail.com>
2019-11-05 11:52:37 -0800

    Pimp out navigation

--- client/src/containers/Header.js
+++ client/src/containers/Header.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { connect} from 'react-redux';
+import { connect } from 'react-redux';
+
+
 
 class Header extends Component {
+    renderLinks(){
+        if(this.props.auth) {
+            return (
+                <div>
+                    <Link to='/counter'>Counter Page</Link>
+                    <Link to='/signout'>Sign Out</Link>
+                    <Link to='/blogs'>Blogs</Link>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <Link to='/signup'>Sign up</Link>
+                    <Link to='/signin'>Sign in</Link>
+                    <Link to='/blogs'>Blogs</Link>
+                </div>
+            );
+        }
+    }
+
     render() {
         return (
             <div>
-                <Link to='/'>Redux Auth</Link>                
+                <Link to='/'>Redux Auth</Link>   
+                {this.renderLinks()}
             </div>
         )
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth: auth.authenticated };
+}
+
+export default connect(mapStateToProps, null)(Header);
